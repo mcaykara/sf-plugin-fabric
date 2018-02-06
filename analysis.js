c1c1a651e4789e9a7055ca8fd41cc0009a353c2a
doganekici26@gmail.com <Dogan Ekici>
2018-02-06 12:15:04 +0000

    Updated fabric

--- analysis.js
+++ analysis.js
@@ -10,8 +10,10 @@ function Fabric(params){}
 
 /**
  * Initialize Fabric and all provided kits.Only the first call to this method is honored. Subsequent calls are no-ops.
+ * Call this method within your `app.js` and provide the kits you wish to use.
  *
  *     @example
+ *      const Fabric = require('sf-plugin-fabric');
  *      const Crashlytics = require('sf-plugin-fabric/crashlytics');
  *      const Answers = require('sf-plugin-fabric/answers');
  *      Fabric.with([new Crashlytics(),new Answers()]);
--- fabric-iOS.js
+++ fabric-iOS.js
@@ -1,7 +1,12 @@
 function Fabric(params) {}
 
 Fabric.with = function(kits){
-    
+    var kitsStringArray = [];
+    for (var kit in kits){
+        kitsStringArray.push(kits[kit].constructor.name);
+    }
+    __SF_Fabric.withStringArray(kitsStringArray);
 }
 
 module && (module.exports = Fabric);
+
