7a8c580daf930c17f2b11c4bdadf9d0d5133d516
msmete <metehansemiz@gmail.com>
2018-02-09 09:53:20 +0300

    Update fabric-Android.js

--- fabric-Android.js
+++ fabric-Android.js
@@ -1,16 +1,20 @@
 function Fabric(params) {}
 
+const AndroidConfig = require("sf-core/util/Android/androidconfig");
+
 Fabric.NativeClass = requireClass("io.fabric.sdk.android.Fabric");
 
 Fabric.with = function(kits){
-	var arr = [];
-	for(var i = 0 ; i<kits.length ; i++){
-		arr.push(new kits[i].nativeClass());
-	}
+	if(!AndroidConfig.isEmulator){
+		var arr = [];
+		for(var i = 0 ; i<kits.length ; i++){
+			arr.push(new kits[i].nativeClass());
+		}
 
-	const AndroidConfig = require('sf-core/util/Android/androidconfig');
-	var activity = AndroidConfig.activity;
-    Fabric.NativeClass.with(activity, array(arr, "io.fabric.sdk.android.Kit"));
+		const AndroidConfig = require('sf-core/util/Android/androidconfig');
+		var activity = AndroidConfig.activity;
+	    Fabric.NativeClass.with(activity, array(arr, "io.fabric.sdk.android.Kit"));
+	}	
 };
 
 
