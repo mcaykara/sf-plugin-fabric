1e0a85c2db830844699dfe4f42e20083710c0ef7
msmete <metehansemiz@gmail.com>
2018-02-09 09:53:05 +0300

    Update crashlytics-Android.js

--- crashlytics/crashlytics-Android.js
+++ crashlytics/crashlytics-Android.js
@@ -2,42 +2,55 @@ function Crashlytics() {
 	this.nativeClass = Crashlytics.NativeClass;
 }
 
+const AndroidConfig = require("sf-core/util/Android/androidconfig");
+
 Crashlytics.NativeClass = requireClass("com.crashlytics.android.Crashlytics");
 
 Crashlytics.setUserIdentifier = function(identifier){
-    Crashlytics.NativeClass.setUserIdentifier(identifier);
+    if(!AndroidConfig.isEmulator)
+        Crashlytics.NativeClass.setUserIdentifier(identifier);
 };
 
 Crashlytics.setUserName = function(name){
-    Crashlytics.NativeClass.setUserName(name);
+    if(!AndroidConfig.isEmulator)
+        Crashlytics.NativeClass.setUserName(name);
 };
 
 Crashlytics.setUserEmail = function(email){
-    Crashlytics.NativeClass.setUserEmail(email);
+    if(!AndroidConfig.isEmulator)
+        Crashlytics.NativeClass.setUserEmail(email);
 };
 
 Crashlytics.setString = function(key, value){
-    Crashlytics.NativeClass.setString(key, value);
+    if(!AndroidConfig.isEmulator)
+        Crashlytics.NativeClass.setString(key, value);
 };
 
 Crashlytics.setBool = function(key, value){
-    Crashlytics.NativeClass.setBool(key, value);
+    if(!AndroidConfig.isEmulator)
+        Crashlytics.NativeClass.setBool(key, value);
 };
 
 Crashlytics.setFloat = function(key, value){
-    Crashlytics.NativeClass.setFloat(key, value);
+    if(!AndroidConfig.isEmulator)
+        Crashlytics.NativeClass.setFloat(key, value);
 };
 
 Crashlytics.setInt = function(key, value){
-    Crashlytics.NativeClass.setInt(key, value);
+    if(!AndroidConfig.isEmulator)
+        Crashlytics.NativeClass.setInt(key, value);
 };
 
 Crashlytics.getVersion = function(){
-    return Crashlytics.NativeClass.getInstance().getVersion();
+    if(!AndroidConfig.isEmulator)
+        return Crashlytics.NativeClass.getInstance().getVersion();
+    else
+        return;
 };
 
 Crashlytics.crash = function(){
-    Crashlytics.NativeClass.getInstance().crash();
+    if(!AndroidConfig.isEmulator)
+        Crashlytics.NativeClass.getInstance().crash();
 };
 
 module && (module.exports = Crashlytics);
